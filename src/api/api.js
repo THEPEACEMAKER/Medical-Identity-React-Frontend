c659bd43946c419ceabf323c3f1c8d0b5f54e29f
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-06-30 20:19:13 +0300

    fix navigation showing unAuthorized links

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // Django
+  baseURL: process.env.REACT_APP_BASE_API_URL, // Django
   headers: {
     "Content-Type": "application/json",
   },
