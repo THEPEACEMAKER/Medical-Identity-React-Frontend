1f9d57aa7a9fa254c6ddacb8e19d77e6b55f4e5d
Islam Sulaiman <ma.eslam@yahoo.com>
2023-07-01 00:33:44 +0300

    Make removing doctor appointment interactive

--- src/features/utils/helpers.js
+++ src/features/utils/helpers.js
@@ -1,3 +1,11 @@
+import api from "../../api/api";
+import { useDispatch } from "react-redux";
+import { doctorActions } from "../../store/doctor/doctor-slice";
+
+
+
+
+
 function convertTimeTo12HourFormat(timeStr) {
     // Create a Date object with the time string
     const dateObj = new Date(`2000-01-01T${timeStr}`);
@@ -27,6 +35,36 @@ function convertTimeTo12HourFormat(timeStr) {
     return time12Hour;
 }
 
+
+function fetchDoctorData (dispatch) {
+  
+  const endpoint1 = "/appointment/doctor/list-all/";
+  const endpoint2 = "/appointment/doctor/list/count/status/";
+  
+  Promise.all([
+    api.get(endpoint1),
+    api.get(endpoint2),
+  ])
+    .then(([appointmentsRes, countRes]) => {
+      const appointments = appointmentsRes.data || [];
+      const count = countRes.data || 0;
+  
+      console.log("inside app")
+      console.log(appointments, count);
+      dispatch(doctorActions.replaceApointments({
+        data: appointments.result,
+        appointmentCount: count,
+        availableAppointments:appointments.result,
+        isLoading : false,
+      }));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+
 export const helpers ={
     convertTimeTo12HourFormat,
+    fetchDoctorData
 }
